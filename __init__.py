51b8bb0051e783f3e06f4a275ef45d31ec05bd28
Flynn-Primrose <dmflynnprimrose@outlook.com>
2025-01-21 14:44:14 -0500

    update __init__

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
 '''
-Initialize Zoonosim by importing all modules
+Initialize Zoonosim by importing all modules.
+
+Convention is to use "import zoonosim as zn", and then to use all functions and
+classes directly, e.g. zn.Sim() rather than zn.sim.Sim()
 '''
+
+# Check that requirements are met and set options
+from . import requirements
+from .settings import *
 
 from .version import __version__, __versiondate__, __license__
 if settings.options.verbose:
