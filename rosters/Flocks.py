e60313fea7ecbfc6c0e1ec6ff218b76ce7e4ec61
Flynn-Primrose <DarrenFP@uoguelph.ca>
2025-05-13 15:50:28 -0400

    cleaning up

--- rosters/Flocks.py
+++ rosters/Flocks.py
@@ -1,8 +1,5 @@
 import numpy as np
-import scipy.stats as stats
 import sciris as sc
-import scipy.stats as stats
-from collections import defaultdict
 from .. import version as znv
 from .. import utils as znu
 from .. import defaults as znd
