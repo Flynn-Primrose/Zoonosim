a95bee84c72c25342adb324e6f758f053d9c9ce1
Flynn-Primrose <dmflynnprimrose@outlook.com>
2025-06-03 12:30:08 -0400

    ploting

--- rosters/Water.py
+++ rosters/Water.py
@@ -164,7 +164,7 @@ class Water(Subroster):
 
     def check_uncontaminated(self):
         ''' Check if waterbodies are contaminated and update their states accordingly '''
-        inds = self.check_inds(self.contaminated, self.date_uncontaminated)
+        inds = self.check_inds(~self.contaminated, self.date_uncontaminated)
         if len(inds) > 0:
             self.uncontaminated[inds] = True
             self.contaminated[inds]    = False
