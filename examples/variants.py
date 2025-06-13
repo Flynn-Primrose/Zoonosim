3919fd78d6c1de6b5b85877816c47dd9a979f3fd
Flynn-Primrose <DarrenFP@uoguelph.ca>
2025-06-13 15:18:36 -0400

    added an example using variants

--- examples/variants.py
+++ examples/variants.py
@@ -2,5 +2,25 @@
 
 import Zoonosim as zn
 
-hpai = zn.variant(variant = 'HPAI', days = 0, n_imports = 2 )
-lpai = zn.variant(variant = 'LPAI', days = 0, n_imports = 10)
+hpai = zn.variant(variant = 'HPAI', days = 0, n_imports = 2 , import_type = ['water', 'barn'])
+lpai = zn.variant(variant = 'LPAI', days = 0, n_imports = 10, import_type = ['water', 'barn'])
+
+new_pars = {}
+
+new_pars['n_imports'] = {
+    'human': 0,
+    'flock': 0,
+    'barn': 0,
+    'water': 0,
+}
+new_pars['variants'] = [hpai, lpai]
+
+sim=zn.Sim(pars=new_pars)
+sim.initialize()
+sim.run()
+# Print the results
+sim.plot()
+sim.plot('human')
+sim.plot('flock')
+sim.plot('barn')
+sim.plot('water')
