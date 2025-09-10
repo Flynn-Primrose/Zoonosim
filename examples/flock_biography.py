2cc2c8b767c3b391793298fb5fa60c9d823a6897
Flynn-Primrose <DarrenFP@uoguelph.ca>
2025-09-10 15:44:11 -0400

    common scenarios

---
+++ examples/flock_biography.py
@@ -0,0 +1,37 @@
+import Zoonosim as zn
+
+bio_analyzer = zn.biography(uid = 266, agent_type = 'flock', days = range(1, 150))
+
+sim=zn.Sim(analyzers = bio_analyzer)
+
+new_pars = { 'rand_seed': 24}
+
+sim.update_pars(new_pars)
+
+
+sim.run(reset_seed=False)
+
+# print(sim.agents.flock.event_log)
+
+flock_biography = sim['analyzers'][0]
+
+headline_plots = [
+    'headcount',
+    'total_dead_headcount',
+]
+
+SEIR_plots = [
+    'exposed_headcount',
+    'infectious_headcount',
+    'symptomatic_headcount'
+]
+
+flock_biography.plot(
+    headline_plots,
+    do_show=True,
+)
+
+flock_biography.plot(
+    SEIR_plots,
+    do_show=True,
+)
---
+++ examples/human_biography.py
@@ -0,0 +1,23 @@
+import Zoonosim as zn
+
+bio_analyzer = zn.biography(uid = 44, agent_type = 'human', days = range(1, 150))
+sim=zn.Sim(analyzers = bio_analyzer)
+
+new_pars = { 'rand_seed': 24}
+
+sim.update_pars(new_pars)
+
+sim.run()
+
+# print(sim.agents.human.infection_log)
+
+human_biography = sim['analyzers'][0]
+
+props_to_plot = [
+    'viral_load',
+]
+
+human_biography.plot(
+    props_to_plot,
+    do_show=True,
+)
