f134201e1e44b6c9e1f737cd474d9a28f8faea0b
Flynn-Primrose <DarrenFP@uoguelph.ca>
2025-10-29 16:09:29 -0400

    getting ready for calibration

--- zoonosim/analysis.py
+++ zoonosim/analysis.py
@@ -3,14 +3,17 @@ Additional analysis functions that are not part of the core Zoonosim workflow,
 but which are useful for particular investigations.
 '''
 
+import os
 import sciris as sc
 import numpy as np
+import pylab as pl
 import pandas as pd
 import matplotlib.pyplot as plt
 from . import interventions as zni
-from . import defaults as znd
+from . import misc as znm
 from . import plotting as znplt
 from . import options as zno
+from . import run as znr
 
 
 __all__ = ['Analyzer', 'snapshot', 'biography']
@@ -345,3 +348,414 @@ class biography(Analyzer):
                 axs.legend(loc='best')
 
         return znplt.handle_show_return(fig=fig, do_show=do_show)
+
+
+def import_optuna():
+    ''' A helper function to import Optuna, which is an optional dependency '''
+    try:
+        import optuna as op # Import here since it's slow
+    except ModuleNotFoundError as E: # pragma: no cover
+        errormsg = f'Optuna import failed ({str(E)}), please install first (pip install optuna)'
+        raise ModuleNotFoundError(errormsg)
+    return op
+class Calibration(Analyzer):
+    '''
+    A class to handle calibration of Covasim simulations. Uses the Optuna hyperparameter
+    optimization library (optuna.org), which must be installed separately (via
+    pip install optuna).
+
+    Note: running a calibration does not guarantee a good fit! You must ensure that
+    you run for a sufficient number of iterations, have enough free parameters, and
+    that the parameters have wide enough bounds. Please see the tutorial on calibration
+    for more information.
+
+    Args:
+        sim          (Sim)  : the simulation to calibrate
+        calib_pars   (dict) : a dictionary of the parameters to calibrate of the format dict(key1=[best, low, high])
+        fit_args     (dict) : a dictionary of options that are passed to sim.compute_fit() to calculate the goodness-of-fit
+        par_samplers (dict) : an optional mapping from parameters to the Optuna sampler to use for choosing new points for each; by default, suggest_uniform
+        custom_fn    (func) : a custom function for modifying the simulation; receives the sim and calib_pars as inputs, should return the modified sim
+        n_trials     (int)  : the number of trials per worker
+        n_workers    (int)  : the number of parallel workers (default: maximum
+        total_trials (int)  : if n_trials is not supplied, calculate by dividing this number by n_workers)
+        name         (str)  : the name of the database (default: 'covasim_calibration')
+        db_name      (str)  : the name of the database file (default: 'covasim_calibration.db')
+        keep_db      (bool) : whether to keep the database after calibration (default: false)
+        storage      (str)  : the location of the database (default: sqlite)
+        label        (str)  : a label for this calibration object
+        die          (bool) : whether to stop if an exception is encountered (default: false)
+        verbose      (bool) : whether to print details of the calibration
+        kwargs       (dict) : passed to cv.Calibration()
+
+    Returns:
+        A Calibration object
+
+    **Example**::
+
+        sim = cv.Sim(datafile='data.csv')
+        calib_pars = dict(beta=[0.015, 0.010, 0.020])
+        calib = cv.Calibration(sim, calib_pars, total_trials=100)
+        calib.calibrate()
+        calib.plot()
+
+    New in version 3.0.3.
+    '''
+
+    def __init__(self, sim, calib_pars=None, fit_args=None, custom_fn=None, par_samplers=None,
+                 n_trials=None, n_workers=None, total_trials=None, name=None, db_name=None,
+                 keep_db=None, storage=None, label=None, die=False, verbose=True):
+        super().__init__(label=label) # Initialize the Analyzer object
+
+        import multiprocessing as mp # Import here since it's also slow
+
+        # Handle run arguments
+        if n_trials  is None: n_trials  = 20
+        if n_workers is None: n_workers = mp.cpu_count()
+        if name      is None: name      = 'covasim_calibration'
+        if db_name   is None: db_name   = f'{name}.db'
+        if keep_db   is None: keep_db   = False
+        if storage   is None: storage   = f'sqlite:///{db_name}'
+        if total_trials is not None: n_trials = total_trials/n_workers
+        self.run_args   = sc.objdict(n_trials=int(n_trials), n_workers=int(n_workers), name=name, db_name=db_name, keep_db=keep_db, storage=storage)
+
+        # Handle other inputs
+        self.sim          = sim
+        self.calib_pars   = calib_pars
+        self.fit_args     = sc.mergedicts(fit_args)
+        self.par_samplers = sc.mergedicts(par_samplers)
+        self.custom_fn    = custom_fn
+        self.die          = die
+        self.verbose      = verbose
+        self.calibrated   = False
+
+        # Handle if the sim has already been run
+        if self.sim.complete:
+            warnmsg = 'Sim has already been run; re-initializing, but in future, use a sim that has not been run'
+            znm.warn(warnmsg)
+            self.sim = self.sim.copy()
+            self.sim.initialize()
+
+        return
+
+
+    def run_sim(self, calib_pars, label=None, return_sim=False):
+        ''' Create and run a simulation '''
+        sim = self.sim.copy()
+        if label: sim.label = label
+        valid_pars = {k:v for k,v in calib_pars.items() if k in sim.pars}
+        sim.update_pars(valid_pars)
+        if self.custom_fn:
+            sim = self.custom_fn(sim, calib_pars)
+        else:
+            if len(valid_pars) != len(calib_pars):
+                extra = set(calib_pars.keys()) - set(valid_pars.keys())
+                errormsg = f'The following parameters are not part of the sim, nor is a custom function specified to use them: {sc.strjoin(extra)}'
+                raise ValueError(errormsg)
+        try:
+            sim.run()
+            sim.compute_fit(**self.fit_args)
+            if return_sim:
+                return sim
+            else:
+                return sim.fit.mismatch
+        except Exception as E:
+            if self.die:
+                raise E
+            else:
+                warnmsg = f'Encountered error running sim!\nParameters:\n{valid_pars}\nTraceback:\n{sc.traceback()}'
+                znm.warn(warnmsg)
+                output = None if return_sim else np.inf
+                return output
+
+
+    def run_trial(self, trial):
+        ''' Define the objective for Optuna '''
+        pars = {}
+        for key, (best,low,high) in self.calib_pars.items():
+            if key in self.par_samplers: # If a custom sampler is used, get it now
+                try:
+                    sampler_fn = getattr(trial, self.par_samplers[key])
+                except Exception as E:
+                    errormsg = 'The requested sampler function is not found: ensure it is a valid attribute of an Optuna Trial object'
+                    raise AttributeError(errormsg) from E
+            else:
+                sampler_fn = trial.suggest_uniform
+            pars[key] = sampler_fn(key, low, high) # Sample from values within this range
+        mismatch = self.run_sim(pars)
+        return mismatch
+
+
+    def worker(self):
+        ''' Run a single worker '''
+        op = import_optuna()
+        if self.verbose:
+            op.logging.set_verbosity(op.logging.DEBUG)
+        else:
+            op.logging.set_verbosity(op.logging.ERROR)
+        study = op.load_study(storage=self.run_args.storage, study_name=self.run_args.name)
+        output = study.optimize(self.run_trial, n_trials=self.run_args.n_trials)
+        return output
+
+
+    def run_workers(self):
+        ''' Run multiple workers in parallel '''
+        if self.run_args.n_workers > 1: # Normal use case: run in parallel
+            output = sc.parallelize(self.worker, iterarg=self.run_args.n_workers)
+        else: # Special case: just run one
+            output = [self.worker()]
+        return output
+
+
+    def remove_db(self):
+        '''
+        Remove the database file if keep_db is false and the path exists.
+
+        New in version 3.1.0.
+        '''
+        if os.path.exists(self.run_args.db_name):
+            os.remove(self.run_args.db_name)
+            if self.verbose:
+                print(f'Removed existing calibration {self.run_args.db_name}')
+        return
+
+
+    def make_study(self):
+        ''' Make a study, deleting one if it already exists '''
+        op = import_optuna()
+        if not self.run_args.keep_db:
+            self.remove_db()
+        output = op.create_study(storage=self.run_args.storage, study_name=self.run_args.name)
+        return output
+
+
+    def calibrate(self, calib_pars=None, verbose=True, **kwargs):
+        '''
+        Actually perform calibration.
+
+        Args:
+            calib_pars (dict): if supplied, overwrite stored calib_pars
+            verbose (bool): whether to print output from each trial
+            kwargs (dict): if supplied, overwrite stored run_args (n_trials, n_workers, etc.)
+        '''
+        op = import_optuna()
+
+        # Load and validate calibration parameters
+        if calib_pars is not None:
+            self.calib_pars = calib_pars
+        if self.calib_pars is None:
+            errormsg = 'You must supply calibration parameters either when creating the calibration object or when calling calibrate().'
+            raise ValueError(errormsg)
+        self.run_args.update(kwargs) # Update optuna settings
+
+        # Run the optimization
+        t0 = sc.tic()
+        self.make_study()
+        self.run_workers()
+        self.study = op.load_study(storage=self.run_args.storage, study_name=self.run_args.name)
+        self.best_pars = sc.objdict(self.study.best_params)
+        self.elapsed = sc.toc(t0, output=True)
+
+        # Compare the results
+        self.initial_pars = sc.objdict({k:v[0] for k,v in self.calib_pars.items()})
+        self.par_bounds   = sc.objdict({k:np.array([v[1], v[2]]) for k,v in self.calib_pars.items()})
+        self.before = self.run_sim(calib_pars=self.initial_pars, label='Before calibration', return_sim=True)
+        self.after  = self.run_sim(calib_pars=self.best_pars,    label='After calibration',  return_sim=True)
+        self.parse_study()
+
+        # Tidy up
+        self.calibrated = True
+        if not self.run_args.keep_db:
+            self.remove_db()
+        if verbose:
+            self.summarize()
+
+        return self
+
+
+    def summarize(self):
+        ''' Print out results from the calibration '''
+        if self.calibrated:
+            print(f'Calibration for {self.run_args.n_workers*self.run_args.n_trials} total trials completed in {self.elapsed:0.1f} s.')
+            before = self.before.fit.mismatch
+            after = self.after.fit.mismatch
+            print('\nInitial parameter values:')
+            print(self.initial_pars)
+            print('\nBest parameter values:')
+            print(self.best_pars)
+            print(f'\nMismatch before calibration: {before:n}')
+            print(f'Mismatch after calibration:  {after:n}')
+            print(f'Percent improvement:         {((before-after)/before)*100:0.1f}%')
+            return before, after
+        else:
+            print('Calibration not yet run; please run calib.calibrate()')
+            return
+
+
+    def parse_study(self):
+        '''Parse the study into a data frame -- called automatically '''
+        best = self.best_pars
+
+        print('Making results structure...')
+        results = []
+        n_trials = len(self.study.trials)
+        failed_trials = []
+        for trial in self.study.trials:
+            data = {'index':trial.number, 'mismatch': trial.value}
+            for key,val in trial.params.items():
+                data[key] = val
+            if data['mismatch'] is None:
+                failed_trials.append(data['index'])
+            else:
+                results.append(data)
+        print(f'Processed {n_trials} trials; {len(failed_trials)} failed')
+
+        keys = ['index', 'mismatch'] + list(best.keys())
+        data = sc.objdict().make(keys=keys, vals=[])
+        for i,r in enumerate(results):
+            for key in keys:
+                if key not in r:
+                    warnmsg = f'Key {key} is missing from trial {i}, replacing with default'
+                    znm.warn(warnmsg)
+                    r[key] = best[key]
+                data[key].append(r[key])
+        self.data = data
+        self.df = pd.DataFrame.from_dict(data)
+
+        return
+
+
+    def to_json(self, filename=None):
+        '''
+        Convert the data to JSON.
+
+        New in version 3.1.1.
+        '''
+        order = np.argsort(self.df['mismatch'])
+        json = []
+        for o in order:
+            row = self.df.iloc[o,:].to_dict()
+            rowdict = dict(index=row.pop('index'), mismatch=row.pop('mismatch'), pars={})
+            for key,val in row.items():
+                rowdict['pars'][key] = val
+            json.append(rowdict)
+        if filename:
+            sc.savejson(filename, json, indent=2)
+        else:
+            return json
+
+
+    def plot_sims(self, **kwargs):
+        '''
+        Plot sims, before and after calibration.
+
+        New in version 3.1.1: renamed from plot() to plot_sims().
+        '''
+        msim = znr.MultiSim([self.before, self.after])
+        fig = msim.plot(**kwargs)
+        return znplt.handle_show_return(fig=fig)
+
+
+    def plot_trend(self, best_thresh=2):
+        '''
+        Plot the trend in best mismatch over time.
+
+        New in version 3.1.1.
+        '''
+        mismatch = sc.dcp(self.df['mismatch'].values)
+        best_mismatch = np.zeros(len(mismatch))
+        for i in range(len(mismatch)):
+            best_mismatch[i] = mismatch[:i+1].min()
+        smoothed_mismatch = sc.smooth(mismatch)
+        fig = pl.figure(figsize=(16,12), dpi=120)
+
+        ax1 = pl.subplot(2,1,1)
+        pl.plot(mismatch, alpha=0.2, label='Original')
+        pl.plot(smoothed_mismatch, lw=3, label='Smoothed')
+        pl.plot(best_mismatch, lw=3, label='Best')
+
+        ax2 = pl.subplot(2,1,2)
+        max_mismatch = mismatch.min()*best_thresh
+        inds = sc.findinds(mismatch<=max_mismatch)
+        pl.plot(best_mismatch, lw=3, label='Best')
+        pl.scatter(inds, mismatch[inds], c=mismatch[inds], label='Usable indices')
+        for ax in [ax1, ax2]:
+            pl.sca(ax)
+            pl.grid(True)
+            pl.legend()
+            sc.setylim()
+            sc.setxlim()
+            pl.xlabel('Trial number')
+            pl.ylabel('Mismatch')
+        return znplt.handle_show_return(fig=fig)
+
+
+    def plot_all(self): # pragma: no cover
+        '''
+        Plot every point in the calibration. Warning, very slow for more than a few hundred trials.
+
+        New in version 3.1.1.
+        '''
+        g = pairplotpars(self.data, color_column='mismatch', bounds=self.par_bounds)
+        return g
+
+
+    def plot_best(self, best_thresh=2): # pragma: no cover
+        ''' Plot only the points with lowest mismatch. New in version 3.1.1. '''
+        max_mismatch = self.df['mismatch'].min()*best_thresh
+        inds = sc.findinds(self.df['mismatch'].values <= max_mismatch)
+        g = pairplotpars(self.data, inds=inds, color_column='mismatch', bounds=self.par_bounds)
+        return g
+
+
+    def plot_stride(self, npts=200): # pragma: no cover
+        '''
+        Plot a fixed number of points in order across the results.
+
+        New in version 3.1.1.
+        '''
+        npts = min(len(self.df), npts)
+        inds = np.linspace(0, len(self.df)-1, npts).round()
+        g = pairplotpars(self.data, inds=inds, color_column='mismatch', bounds=self.par_bounds)
+        return g
+
+def pairplotpars(data, inds=None, color_column=None, bounds=None, cmap='parula', bins=None, edgecolor='w', facecolor='#F8A493', figsize=(20,16)): # pragma: no cover
+    ''' Plot scatterplots, histograms, and kernel densities for calibration results '''
+    try:
+        import seaborn as sns # Optional import
+    except ModuleNotFoundError as E:
+        errormsg = 'Calibration plotting requires Seaborn; please install with "pip install seaborn"'
+        raise ModuleNotFoundError(errormsg) from E
+
+    data = sc.odict(sc.dcp(data))
+
+    # Create the dataframe
+    df = pd.DataFrame.from_dict(data)
+    if inds is not None:
+        df = df.iloc[inds,:].copy()
+
+    # Choose the colors
+    if color_column:
+        colors = sc.vectocolor(df[color_column].values, cmap=cmap)
+    else:
+        colors = [facecolor for i in range(len(df))]
+    df['color_column'] = [sc.rgb2hex(rgba[:-1]) for rgba in colors]
+
+    # Make the plot
+    grid = sns.PairGrid(df)
+    grid = grid.map_lower(pl.scatter, **{'facecolors':df['color_column']})
+    grid = grid.map_diag(pl.hist, bins=bins, edgecolor=edgecolor, facecolor=facecolor)
+    grid = grid.map_upper(sns.kdeplot)
+    grid.fig.set_size_inches(figsize)
+    grid.fig.tight_layout()
+
+    # Set bounds
+    if bounds:
+        for ax in grid.axes.flatten():
+            xlabel = ax.get_xlabel()
+            ylabel = ax.get_ylabel()
+            if xlabel in bounds:
+                ax.set_xlim(bounds[xlabel])
+            if ylabel in bounds:
+                ax.set_ylim(bounds[ylabel])
+
+    return grid    
